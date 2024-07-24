506fc49f8a3081e87b86eaf02e0d3bd0ce47d27e
Ruban Eugeny <schiller2k@gmail.com>
2024-07-24 01:54:48 +0000

    lesson5

--- lesson5/task2/server.sh
+++ lesson5/task2/server.sh
@@ -7,7 +7,7 @@ sudo iptables -A INPUT -p icmp -j DROP
 
 echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections
 echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections
-sudo apt-get -y install iptables-persistent
+sudo apt install -y iptables-persistent
 sudo iptables-save > /etc/iptables/rules.v4
 
 sudo apt install -y openssh-server
