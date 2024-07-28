a44092e3b220ec99044518f584bd9cfc974f04d5
Ruban Eugeny <schiller2k@gmail.com>
2024-07-28 12:32:08 +0000

    lesson8

--- lesson8/client.sh
+++ lesson8/client.sh
@@ -2,16 +2,8 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-DIR=/home/vagrant/.ssh
+sudo echo "192.168.56.34 test.local www.test.local" >> /etc/hosts
 
-cp /vagrant/authorized_keys $DIR/authorized_keys
-
-chown -R vagrant:vagrant $DIR
-chmod 600 $DIR/authorized_keys
-chmod 700 $DIR
-
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-echo "AllowUsers vagrant@192.168.56.34" | sudo tee -a /etc/ssh/sshd_config
-sudo systemctl restart ssh
+sudo cp /vagrant/selfsigned.crt /usr/local/share/ca-certificates/
+sudo update-ca-certificates
 
